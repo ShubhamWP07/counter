c28efc856fc3cd4b289e1b8ea7c96d3243ce1243
ShubhamWP07 <shubham1900384@gmail.com>
2023-05-24 18:56:29 +0530

    counterapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  // Increment function
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  // Decrement function
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="counter-setion">
+        <h1 className="heading">Counter Web App</h1>
+        <div className="number-section">
+          <h1 className="num">{count}</h1>
+        </div>
+        <div className="btn-section">
+          <button className="btn" onClick={increment}>
+            Increment
+          </button>
+          <button className="btn" onClick={decrement}>
+            decrement
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
